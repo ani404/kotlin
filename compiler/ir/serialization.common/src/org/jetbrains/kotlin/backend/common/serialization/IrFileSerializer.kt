ed0bc2658e4d9ded9b6609374ce07f6f0a4750e6
Johan Bay <jobay@google.com>
2023-06-13 12:38:54 +0200

    Fix review comments and remove coordinates

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrFileSerializer.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrFileSerializer.kt
@@ -25,7 +25,6 @@ import org.jetbrains.kotlin.library.impl.IrMemoryArrayWriter
 import org.jetbrains.kotlin.library.impl.IrMemoryDeclarationWriter
 import org.jetbrains.kotlin.library.impl.IrMemoryStringWriter
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.resolve.descriptorUtil.isEffectivelyPrivateApi
 import org.jetbrains.kotlin.resolve.descriptorUtil.isEffectivelyPublicApi
 import org.jetbrains.kotlin.types.Variance
 import org.jetbrains.kotlin.utils.filterIsInstanceAnd
@@ -191,7 +190,11 @@ open class IrFileSerializer(
     private fun serializeIrStatementOrigin(origin: IrStatementOrigin): Int =
         serializeString((origin as? IrStatementOriginImpl)?.debugName ?: error("Unable to serialize origin ${origin.javaClass.name}"))
 
-    private fun serializeCoordinates(start: Int, end: Int): Long = BinaryCoordinates.encode(start, end)
+    private fun serializeCoordinates(start: Int, end: Int): Long = if (skipPrivateApi) {
+        0L
+    } else {
+        BinaryCoordinates.encode(start, end)
+    }
 
     /* ------- Strings ---------------------------------------------------------- */
 
@@ -1359,6 +1362,8 @@ open class IrFileSerializer(
     open fun keepOrderOfProperties(property: IrProperty): Boolean = !property.isConst
     open fun backendSpecificSerializeAllMembers(irClass: IrClass) = false
 
+    private fun skipIfPrivate(declaration: DeclarationDescriptor) = skipPrivateApi && declaration is DeclarationDescriptorWithVisibility && !declaration.isEffectivelyPublicApi
+
     open fun memberNeedsSerialization(member: IrDeclaration): Boolean {
         val parent = member.parent
         require(parent is IrClass)
@@ -1366,7 +1371,7 @@ open class IrFileSerializer(
         if (bodiesOnlyForInlines && member is IrAnonymousInitializer && parent.visibility != DescriptorVisibilities.LOCAL)
             return false
         val descriptor = member.descriptor
-        if (skipPrivateApi && descriptor is DeclarationDescriptorWithVisibility && !descriptor.isEffectivelyPublicApi) {
+        if (skipPrivateApi && descriptor is DeclarationDescriptorWithVisibility && !descriptor.visibility.isPublicAPI) {
             return false
         }
 
@@ -1431,11 +1436,11 @@ open class IrFileSerializer(
 
         file.declarations.forEach {
             val descriptor = it.descriptor
-            if (skipPrivateApi && descriptor is DeclarationDescriptorWithVisibility && !descriptor.isEffectivelyPublicApi) {
-                topLevelDeclarations.add(SkippedDeclaration)
+            if (skipIfPrivate(descriptor)) {
+                // Skip the declaration if producing header klib and the declaration is not public.
                 return@forEach
             }
-            if (skipExpects && it.descriptor.isExpectMember && !it.descriptor.isSerializableExpectClass) {
+            if (skipExpects && descriptor.isExpectMember && !descriptor.isSerializableExpectClass) {
                 // Skip the declaration unless it is `expect annotation class` marked with `OptionalExpectation`
                 // without the corresponding `actual` counterpart for the current leaf target.
                 return@forEach
@@ -1457,7 +1462,7 @@ open class IrFileSerializer(
 
         // Make sure that all top level properties are initialized on library's load.
         file.declarations
-            .filterIsInstanceAnd<IrProperty> { it.backingField?.initializer != null && keepOrderOfProperties(it) }
+            .filterIsInstanceAnd<IrProperty> { it.backingField?.initializer != null && keepOrderOfProperties(it) && !skipIfPrivate(it.descriptor) }
             .forEach {
                 val fieldSymbol = it.backingField?.symbol ?: error("Not found ID ${it.render()}")
                 proto.addExplicitlyExportedToCompiler(serializeIrSymbol(fieldSymbol))
