c2f09d2a12f31038cab8d42735c59dba474fc91c
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2023-07-13 13:55:12 +0200

    [LL FIR] KT-58257 Turn `LLFirSession.isValid` into a volatile variable
    - We do not need `AtomicBoolean` here, because setting `_isValid` to a
      constant `false` suffices.
    - Also replace the public setter of `isValid` with `markInvalid` to
      prohibit setting `isValid` to `true` again.

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirSessionCache.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirSessionCache.kt
@@ -88,7 +88,7 @@ class LLFirSessionCache(private val project: Project) {
 
     private fun removeSessionFrom(module: KtModule, storage: SessionStorage): Boolean {
         val session = storage.remove(module) ?: return false
-        session.isValid = false
+        session.markInvalid()
         return true
     }
 
@@ -124,7 +124,7 @@ class LLFirSessionCache(private val project: Project) {
     private fun removeAllSessionsFrom(storage: SessionStorage) {
         // Because `removeAllSessionsFrom` is executed in a write action, the order of setting `isValid` and clearing `storage` is not
         // important.
-        storage.values.forEach { it.isValid = false }
+        storage.values.forEach { it.markInvalid() }
         storage.clear()
     }
 
@@ -135,7 +135,7 @@ class LLFirSessionCache(private val project: Project) {
         // "collect and remove" approach also works.
         val scriptEntries = storage.entries.filter { (module, _) -> shouldBeRemoved(module) }
         for ((module, session) in scriptEntries) {
-            session.isValid = false
+            session.markInvalid()
             storage.remove(module)
         }
     }
