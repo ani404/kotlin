46d1f6d32431c09f7095837a27b64190b8519d27
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-07-20 14:37:09 +0200

    Remove FILE target from JsName annotation and use the new experimental JsFileName annotation

--- libraries/stdlib/wasm/src/kotlin/js/annotations.kt
+++ libraries/stdlib/wasm/src/kotlin/js/annotations.kt
@@ -30,7 +30,6 @@ public actual annotation class JsExport {
  */
 @Retention(AnnotationRetention.BINARY)
 @Target(
-    AnnotationTarget.FILE,
     AnnotationTarget.CLASS,
     AnnotationTarget.FUNCTION,
     AnnotationTarget.PROPERTY,
