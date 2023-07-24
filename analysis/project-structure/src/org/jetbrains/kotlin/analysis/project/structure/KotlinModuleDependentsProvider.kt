f047e5dbad01cad9ba2db39f1c663c2668db3f40
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2023-07-24 13:25:44 +0200

    fixup! [AA] KT-58257 Add module dependents exception for `KtSdkModule`

--- analysis/project-structure/src/org/jetbrains/kotlin/analysis/project/structure/KotlinModuleDependentsProvider.kt
+++ analysis/project-structure/src/org/jetbrains/kotlin/analysis/project/structure/KotlinModuleDependentsProvider.kt
@@ -10,9 +10,10 @@ import com.intellij.openapi.project.Project
 /**
  * [KotlinModuleDependentsProvider] provides dependents for a [KtModule], which are modules that depend on the [KtModule].
  *
- * [getDirectDependents] and [getTransitiveDependents] may return an empty set for [KtSdkModule]s even though most modules depend on an SDK,
- * because it is often not feasible to compute that set. Instead, users of [KotlinModuleDependentsProvider] should keep this limitation in
- * mind and handle it separately, for example by using global modification events for [KtSdkModule] changes.
+ * [getDirectDependents] and [getTransitiveDependents] may return an empty set for [KtBuiltinsModule]s and [KtSdkModule]s even though most
+ * modules depend on builtins/SDKs, because it is often not feasible to compute that set. Instead, users of [KotlinModuleDependentsProvider]
+ * should keep this limitation in mind and handle it separately. For example, a global modification event should be published for builtins
+ * and SDK changes.
  *
  * Implementations of this provider should ensure that results are provided in reasonable time, for example by caching results, as its
  * functions may be called frequently.
